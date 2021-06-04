e915917bd4222be23947df7a0596e76722ebe057
markmayo28 <78807451+markmayo28@users.noreply.github.com>
2021-06-04 15:16:59 -0400

    Add files via upload

--- Queries/schema_empDB.sql
+++ Queries/schema_empDB.sql
@@ -45,14 +45,15 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
+--DROP TABLE titles;
+
 CREATE TABLE titles (
 emp_no INT NOT NULL,
 	title VARCHAR(40) NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no), 
-    PRIMARY KEY (emp_no)
+    PRIMARY KEY (emp_no, from_date)
 );
 
-SELECT * FROM departments;
-
+SELECT * FROM titles;
